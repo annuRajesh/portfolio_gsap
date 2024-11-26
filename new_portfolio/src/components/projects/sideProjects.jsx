f43313694dead54c74fe1d0482f88c5d39efd49d
Annu Rajesh <joannetsengannu@gmail.com>
2024-11-26 10:09:13 +0530

    feat

--- new_portfolio/src/components/projects/sideProjects.jsx
+++ new_portfolio/src/components/projects/sideProjects.jsx
@@ -38,9 +38,9 @@ const SideProjects = () => {
   return (
     <div className='project w-full h-full bg-black p-4 flex flex-col justify-center items-center gap-8 max-lg:h-full overflow-hidden'>
         <h2 className='heading text-white text-8xl text-center'>Projects</h2>
-       <div className=" flex flex-wrap  justify-center gap-6">
+       <div className="card flex flex-wrap  justify-center gap-6">
        {
-            projects.map((item,index)=><div className="card w-1/4 max-lg:w-1/3 max-md:w-1/2 max-sm:w-full" key={index}>
+            projects.map((item,index)=><div className=" w-1/4 max-lg:w-1/3 max-md:w-1/2 max-sm:w-full" key={index}>
                 <ProjectCard title={item.title} description={item.description} weblink={item.weblink} gitlink={item.gitlink}/>
             </div>
             )
